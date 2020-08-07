64202f5fdd6e95101f3f903f8092b15b9c1b587a
tejeshreddy <tejeshreddy111@gmail.com>
2020-08-07 18:11:25 +0530

    fix: models.py Tag to Product

--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -11,6 +11,12 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     CATEGORY = (
         ('Indoor', 'Indoor'),
@@ -19,9 +25,13 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-
+    tags = models.ManyToManyField(Tag)
+    
+    def __str__(self):
+        return self.name
+    
 class Order(models.Model):
     
     STATUS = (
@@ -30,7 +40,8 @@ class Order(models.Model):
         ('Delivered', 'Delivered')
     )
     
-    # customer =
-    # Product = 
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    Product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
+    
