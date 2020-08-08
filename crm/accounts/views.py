6f92f3f6c003877498db85decc2f7236cd2ce650
tejeshreddy <tejeshreddy111@gmail.com>
2020-08-08 17:49:18 +0530

    update: creteOrder view

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import OrderForm
 
 # Create your views here.
 
@@ -38,3 +39,14 @@ def customer(request, pk):
                'orders_count': orders_count
                }
     return render(request, 'accounts/customer.html', context)
+
+def createOrder(request):
+    form = OrderForm()
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        
+    context = {'form': form}
+    return render(request, 'accounts/order_form.html', context)
