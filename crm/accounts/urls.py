591da098303b0fd71b871e08c38edd10de7ae055
tejeshreddy <tejeshreddy111@gmail.com>
2020-08-08 16:03:30 +0530

    update: URL's changed

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('products/', views.products),
-    path('customer/', views.customer),
+    path('', views.home, name='home'),
+    path('products/', views.products, name='products' ),
+    path('customer/<str:pk>', views.customer, name='customer'),
 ]
-
