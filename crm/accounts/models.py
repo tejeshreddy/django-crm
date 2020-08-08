7af8f79e15c1ebe211be2d43c07f4a4afda6d280
tejeshreddy <tejeshreddy111@gmail.com>
2020-08-08 16:03:13 +0530

    fix: FK fixed

--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -41,7 +41,6 @@ class Order(models.Model):
     )
     
     customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
-    Product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
-    
