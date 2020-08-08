0a2505c6839870007b9a9c26c3554a918cfb2f31
tejeshreddy <tejeshreddy111@gmail.com>
2020-08-08 17:49:00 +0530

    update: create_order URL

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.products, name='products' ),
     path('customer/<str:pk>', views.customer, name='customer'),
+    path('create_order', views.createOrder, name='create_order')
 ]
