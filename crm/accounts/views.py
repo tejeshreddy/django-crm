a3ea6bed36aa7232e1fad1a4d95a6f6286cca8d9
tejeshreddy <tejeshreddy111@gmail.com>
2020-08-08 22:24:04 +0530

    update: inline forms and delete orders

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 from .models import *
 from .forms import OrderForm
 
@@ -40,13 +41,41 @@ def customer(request, pk):
                }
     return render(request, 'accounts/customer.html', context)
 
-def createOrder(request):
-    form = OrderForm()
+def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=['product', 'status'], extra=10)
+    customer = Customer.objects.get(id=pk)
+    
+    formset = OrderFormSet(queryset=Order.objects.none() ,instance=customer)
+    # form = OrderForm(initial={'customer': customer})
     if request.method == 'POST':
-        form = OrderForm(request.POST)
+        # form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
+            return redirect('/')
+        
+    context = {'formset': formset}
+    return render(request, 'accounts/order_form.html', context)
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+    
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
         if form.is_valid():
             form.save()
             return redirect('/')
-        
+    
     context = {'form': form}
     return render(request, 'accounts/order_form.html', context)
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == 'POST':
+        order.delete()
+        return redirect("/")
+    context = {
+        'item': order
+    }
+    return render(request, 'accounts/delete.html', context)
