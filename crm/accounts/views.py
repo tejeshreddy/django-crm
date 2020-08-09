f781734e9ec6d9bf149056b9def7bb5239afcd57
tejeshreddy <tejeshreddy111@gmail.com>
2020-08-09 22:31:55 +0530

    update: view for create and register user

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
 from .models import *
-from .forms import OrderForm
+from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
 
 # Create your views here.
+
+def registerPage(request):
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for' + user)
+            return redirect('login')
+        
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
+
+def loginPage(request):
+    context = {}
+    return render(request, 'accounts/login.html', context)
+
 
 def home(request):
     orders = Order.objects.all()
