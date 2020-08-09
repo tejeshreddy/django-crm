4419ebaf5497ebba9c9c0e6f653e25df6b01479f
tejeshreddy <tejeshreddy111@gmail.com>
2020-08-09 22:31:27 +0530

    update: URL for register and create

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('register/', views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login"),    
     path('', views.home, name='home'),
     path('products/', views.products, name='products' ),
     path('customer/<str:pk>', views.customer, name='customer'),
